9b508a72444f67310456a92deb6672d6ec9ad133
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-04-29 21:33:20 -0400

    add pool size attribute

--- scripts/custommpipool.py
+++ scripts/custommpipool.py
@@ -56,6 +56,8 @@ class MPIPoolExecutor(Executor):
         self._lock = threading.Lock()
         self._pool = None
 
+        self.size = self._comm.Get_size() - 1
+
     _make_pool = staticmethod(_lib.WorkerPool)
 
     def _bootstrap(self):
